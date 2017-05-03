e28b450b79b16d855eb5e327f67f1a3cb1da9e1d
TOR- <tiarnachreidy@gmail.com>
2017-05-03 12:41:07 +0100

    Kernel start and end markers to determine size

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -12,6 +12,8 @@
 #include <sys/io.h>
 #include <sys/debug.h>
 
+void kernel_end();
+
 void kernel_main(void) {  
   //serial_initialise();
   serial_writes(__DATE__ "\t" __TIME__ "\n");
@@ -26,9 +28,20 @@ void kernel_main(void) {
  __asm__ __volatile__ ("sti");
  
   printf("Well, Chuckles\nChuckle away\n");
-  //init_paging();
+  // init_paging();
+  printf("JKIBJONJ\n");
+  kernel_end();
 }
 
+extern unsigned long kernel_end_marker;
+extern unsigned long kernel_start_marker;
+void kernel_end()
+{
+  printf("kernel start = %d\n", &kernel_start_marker); 
+  printf("kernel end   = %d\n",  kernel_end_marker);
+  printf("kernel ends at %d\n", &kernel_end_marker);
+  printf("kernel size: B = %d\tkB = %d", &kernel_end_marker - &kernel_start_marker, (&kernel_end_marker - &kernel_start_marker)/1024);
+}
 /*
  * Old tests:
  printf("%d", 1/0);
