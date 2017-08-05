916745a780654330f4b529fa42b3171927758fce
TOR- <tiarnachreidy@gmail.com>
2017-08-05 12:20:44 +0100

    Grub module files

--- iso.sh
+++ iso.sh
@@ -5,11 +5,15 @@ set -e
 mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
+mkdir -p isodir/modules
 
 cp sysroot/boot/chuckles.kernel isodir/boot/chuckles.kernel
+cp -R modules isodir/
+
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "chuckles" {
 	multiboot /boot/chuckles.kernel
+	module /modules/program
 }
 EOF
 grub-mkrescue -o chuckles.iso isodir
