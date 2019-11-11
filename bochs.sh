6482c68a6553c866f94d320f8f498307b8224315
TOR- <tiarnachreidy@gmail.com>
2019-11-11 15:07:53 +0000

    Bochs script

---
+++ bochs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+. ./iso.sh
+
+#cpulimit -l 10 -- qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom chuckles.iso -monitor stdio -serial file:serial.log -d int
+#qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom chuckles.iso -monitor stdio -serial file:serial.log -singlestep -d int 2>&1 | tee qemu.log
+bochs -f bochsrc -q
