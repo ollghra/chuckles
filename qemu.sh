336bf90a009533671154551453ed0de7636e3e8d
TOR- <tiarnachreidy@gmail.com>
2019-12-18 11:12:26 +0000

    Hardware accelerated qemu

--- qemu.sh
+++ qemu.sh
@@ -3,4 +3,5 @@ set -e
 . ./iso.sh
 
 #cpulimit -l 10 -- qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom chuckles.iso -monitor stdio -serial file:serial.log -d int
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom chuckles.iso -monitor stdio -serial file:serial.log -singlestep -d int 2>&1 | tee qemu.log
+#qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom chuckles.iso -monitor stdio -serial file:serial.log -singlestep -d int 2>&1 | tee qemu.log
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -machine pc,accel=kvm  -cdrom chuckles.iso -monitor stdio -serial file:serial.log -singlestep -d int,pcall,guest_errors,cpu_reset -D qemu.log 2>&1|tee qemu.log
