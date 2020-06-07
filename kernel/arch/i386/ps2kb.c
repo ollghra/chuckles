f0143d2ec1361d09cdc513e142afe09f09900f63
Tiarnach O Riada <tiarnachreidy@gmail.com>
2020-06-07 16:26:57 +0100

    Modifier keys working

--- kernel/arch/i386/ps2kb.c
+++ kernel/arch/i386/ps2kb.c
@@ -10,12 +10,34 @@
 #include <sys/kb.h>
 #include <sys/shell.h>
 
-#define LCTL 0x10
-#define LSHF 0x11
-#define LALT 0x12
+#define LCTL 0x14
+#define LALT 0x11
+#define LSHF 0x12
 #define RCTL LCTL
-#define RSHF 0x14 
+#define RSHF 0x59
 #define RALT LALT
+
+#define MOD  0x1F
+
+#define KUA  KEY_UP_ARROW
+#define KDA  KEY_DOWN_ARROW
+#define KRA  KEY_RIGHT_ARROW
+#define KLA  KEY_LEFT_ARROW
+#define KPU  KEY_PAGE_UP
+#define KPD  KEY_PAGE_DOWN
+
+#define F1  0x05
+#define F2  0x06
+#define F3  0x04
+#define F4  0x0C
+#define F5  0x03
+#define F6  0x0B
+#define F7  0x83
+#define F8  0x0A // 0x0A conflicted with \n
+#define F9  0x01
+#define F10 0x09
+#define F11 0x78
+#define F12 0x07
 
 // Different keyset
 #define KS_DEFAULT 0
@@ -28,23 +50,24 @@ struct {
     bool rctrl;
     bool lalt;
     bool ralt;
+    bool mod;
 } ps2kb_state;
 
-char (*kb_map)[2][256];
+unsigned char (*kb_map)[2][256];
 
-char gb[2][256] = { // GB keyboard mapping
+unsigned char gb[2][256] = { // GB keyboard mapping
     {
         /* Unmodified keyset
-           0   1    2    3    4    5    6    7    8    9    A    B    C    D    E    F  */
-        0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,'\t', 0  , 0  , // 0
-        0  ,LALT,LSHF, 0  ,LCTL, 'q', '1', 0  , 0  , 0  , 'z', 's', 'a', 'w', '2', 0  , // 1
+         0   1    2    3    4    5    6    7    8    9    A    B    C    D    E    F  */
+        0  , F9 , 0  , F5 , F3 , F1 , F2 , F12, 0  , F10, F8 , F6 , F4 ,'\t', '`', 0  , // 0
+        0  ,LALT,LSHF, 0  ,LCTL, 'q', '1', 0  , 0  , 0  , 'z', 's', 'a', 'w', '2', MOD, // 1
         0  , 'c', 'x', 'd', 'e', '4', '3', 0  , 0  , ' ', 'v', 'f', 't', 'r', '5', 0  , // 2
         0  , 'n', 'b', 'h', 'g', 'y', '6', 0  , 0  , 0  , 'm', 'j', 'u', '7', '8', 0  , // 3
         0  , ',', 'k', 'i', 'o', '0', '9', 0  , 0  , '.', '/', 'l', ';', 'p', '-', 0  , // 4
         0  , 0  ,'\'', 0  , '[', '=', 0  , 0  , 0  ,RSHF,'\n', ']', 0  , '#', 0  , 0  , // 5
-        0  ,'\\', 0  , 0  , 0  , 0  ,'\b', 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , // 6
-        0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , // 7
-        0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , // 8
+        0  ,'\\', 0  , 0  , 0  , 0  ,'\b', 0  , 0  , 0  , 0  , KLA, 0  , 0  , 0  , 0  , // 6
+        0  , 0  , KDA, 0  , KRA, KUA, 0  , 0  , F11, 0  , KPD, 0  , 0  , KPU, 0  , 0  , // 7
+        0  , 0  , 0  , F7 , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , // 8
         0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , // 9
         0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , // A
         0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , // B
@@ -55,8 +78,8 @@ char gb[2][256] = { // GB keyboard mapping
     },
     {
         /* SHIFT held down
-           0   1    2    3    4    5    6    7    8    9    A    B    C    D    E    F  */
-        0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,'\t', 0  , 0  , // 0
+         0   1    2    3    4    5    6    7    8    9    A    B    C    D    E    F  */
+        0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,'\t', '¬', 0  , // 0
         0  ,LALT,LSHF, 0  ,LCTL, 'Q', '!', 0  , 0  , 0  , 'Z', 'S', 'A', 'W', '"', 0  , // 1
         0  , 'C', 'X', 'D', 'E', '$', '£', 0  , 0  , ' ', 'V', 'F', 'T', 'R', '%', 0  , // 2
         0  , 'N', 'B', 'H', 'G', 'Y', '^', 0  , 0  , 0  , 'M', 'J', 'U', '&', '*', 0  , // 3
@@ -80,9 +103,8 @@ void ps2kb_handler(struct regs *r)
     uint8_t scancode = 0;
     scancode = ps2_read();
 
-    struct key_event e;
-    e.flags = 0x1; // Set special bit by default
-    bool state;
+    struct key_event e = {0, KEY_NONE, KB_FLAG_SPECIAL};
+    bool state = true; // state { true == down, false == up }
     if(scancode == 0xF0)
     {
         state = false;
@@ -100,7 +122,7 @@ void ps2kb_handler(struct regs *r)
         {
             state = true;
         }
-        switch((*kb_map)[KS_DEFAULT][scancode])
+        switch(scancode)//(*kb_map)[KS_DEFAULT][scancode])
         {
             case RCTL:
                 ps2kb_state.rctrl = state;
@@ -108,25 +130,28 @@ void ps2kb_handler(struct regs *r)
             case RALT:
                 ps2kb_state.ralt  = state;
                 break;
+            case MOD:
+                ps2kb_state.mod = state;
+                break;
+            case 0:
+                printf("<Unhandled E0 scancode %x>", scancode);
+                break;
             default:
-                printf("Unhandled E0 scancode %x", scancode);
+                e.code = scancode;
                 break;
         }
     }
     else if(scancode == 0)
     {
-        printf("WHY 0?");
+        printf("0 scancode rx");
     }
     else
     {
         state = true;
     }
 
-    switch((*kb_map)[KS_DEFAULT][scancode])
+    switch(scancode)
     {
-        case 0:
-            printf("<%x:%d>", scancode,__LINE__);
-            break;
         case LSHF:
             ps2kb_state.lshift = state;
             break;
@@ -139,28 +164,66 @@ void ps2kb_handler(struct regs *r)
         case RSHF:
             ps2kb_state.rshift = state;
             break;
+        case MOD:
+            ps2kb_state.mod = state;
+            break;
+        case F1 :
+        case F2 :
+        case F3 :
+        case F4 :
+        case F5 :
+        case F6 :
+        case F7 :
+        case F8 :
+        case F9 :
+        case F10:
+        case F11:
+        case F12:
+            // TODO: do something with function keys
+            break;
         default:
-            // Find utf-8 equivalent if not modified
-            e.flags = 0 + // Special bit unset, i.e. character
-                (((int)(ps2kb_state.lctrl || ps2kb_state.rctrl)) << 1) +
-                (((int)(ps2kb_state.lshift || ps2kb_state.rshift)) << 2) +
-                (((int)(ps2kb_state.lalt || ps2kb_state.ralt )) << 3) +
-                ((int)state << 7);
-            if (e.flags & KB_FLAG_CONTROL || e.flags & KB_FLAG_ALT)
+            switch((*kb_map)[KS_DEFAULT][scancode])
             {
-                e.utf8 = 0;
-                e.flags &= KB_FLAG_SPECIAL;
-            }
-            else if (e.flags & KB_FLAG_SHIFT)
-            {
-                e.utf8 = (*kb_map)[KS_SHIFT][scancode];
-            }
-            else
-            {
-                e.utf8 = (*kb_map)[KS_DEFAULT][scancode];
-            }
+                case 0:
+                    printf("<Unmapped scancode %x>", scancode);
+                    break;
+                default:
+                    // Find utf-8 equivalent if not modified
+                    e.flags |= 
+                        (((int)(ps2kb_state.lctrl || ps2kb_state.rctrl)) << 1) +
+                        (((int)(ps2kb_state.lshift || ps2kb_state.rshift)) << 2) +
+                        (((int)(ps2kb_state.lalt || ps2kb_state.ralt )) << 3) +
+                        ((int)ps2kb_state.mod << 4) +
+                        ((int)state << 7);
+                    if (e.flags & KB_FLAG_CONTROL || e.flags & KB_FLAG_ALT || e.flags & KB_FLAG_MOD)
+                    {
+                        // These keys send modified letters
+                        e.utf8 = (*kb_map)[KS_DEFAULT][scancode];
+                        e.flags |= KB_FLAG_SPECIAL;
+                    }
+                    else if (e.code != KEY_NONE)
+                    {
+                        // For non-printable keys (arrows, etc.) with code set
+                        e.utf8 = 0;
+                        e.flags |= KB_FLAG_SPECIAL;
+                    }
+                    else if (e.flags & KB_FLAG_SHIFT)
+                    {
+                        e.utf8 = (*kb_map)[KS_SHIFT][scancode];
+                        e.flags &= ~KB_FLAG_SPECIAL;
+                    }
+                    else
+                    {
+                        e.utf8 = (*kb_map)[KS_DEFAULT][scancode];
+                        e.flags &= ~KB_FLAG_SPECIAL;
+                    }
+                    break;
+             }
             break;
     }
+
+    e.code=scancode;
+    //printf("%d:<%c(%x):%x:%x>\n",__LINE__,e.utf8,e.utf8,e.code,e.flags);
 
     shell_key(e);
     // Fire key_event ==> >=3 }-->
@@ -187,6 +250,7 @@ void ps2kb_init(void)
 
     kb_map = &gb;
 
+    // TODO: determine cause of delay before output
     serial_writes("ps2 keyboard installed [WARNING! THIS REALLY DOESN't WORK]\n");
 }
 
--- libc/include/sys/kb.h
+++ libc/include/sys/kb.h
@@ -1,16 +1,24 @@
 #ifndef _SYS_KB_H
 #define _SYS_KB_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef enum {
+    KEY_NONE         =0x00,
+
+    KEY_UP_ARROW     =0x75,
+    KEY_DOWN_ARROW   =0x72,
+    KEY_LEFT_ARROW   =0x6B,
+    KEY_RIGHT_ARROW  =0x74,
+
+    KEY_PAGE_UP      =0x7D,
+    KEY_PAGE_DOWN    =0x7A,
+} KEYCODE;
+
 struct key_event {
-  uint8_t utf8;
-  //bool down;
+  uint8_t utf8; // Printable representation, 0 for special keys
+  KEYCODE code; // keycode as defined below
   uint8_t flags; // LSB[special, control, shift, alt, 0,0,0, downMSB]
 };
 
@@ -18,10 +26,7 @@ struct key_event {
 #define KB_FLAG_CONTROL (1<<1)
 #define KB_FLAG_SHIFT   (1<<2)
 #define KB_FLAG_ALT     (1<<3)
+#define KB_FLAG_MOD     (1<<4)
 #define KB_FLAG_DOWN    (1<<7)
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
