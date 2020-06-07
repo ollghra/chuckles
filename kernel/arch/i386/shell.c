07b90984f90f5eaacdf829e24a03837be765f3bf
Tiarnach O Riada <tiarnachreidy@gmail.com>
2020-06-07 16:26:57 +0100

    Modifier keys working

--- kernel/arch/i386/shell.c
+++ kernel/arch/i386/shell.c
@@ -5,9 +5,36 @@
 
 int shell_key(struct key_event e)
 {
-    if(~e.flags & KB_FLAG_SPECIAL && e.flags & KB_FLAG_DOWN)
+    if(!(e.flags & KB_FLAG_SPECIAL) && e.flags & KB_FLAG_DOWN)
     {
         printf("%c", e.utf8);
+    }
+    else if (e.flags & KB_FLAG_DOWN){
+        printf("%c%c%c%c+", e.flags & KB_FLAG_SHIFT ? 's':' ',e.flags & KB_FLAG_CONTROL ? 'c':' ',  e.flags & KB_FLAG_ALT ? 'a':' ', e.flags & KB_FLAG_MOD ? 'm':' ');
+        switch (e.code)
+        {
+            case KEY_UP_ARROW:
+                printf("<UP>");
+                break;
+            case KEY_DOWN_ARROW:
+                printf("<DOWN>");
+                break;
+            case KEY_LEFT_ARROW:
+                printf("<LEFT>");
+                break;
+            case KEY_RIGHT_ARROW:
+                printf("<RIGHT>");
+                break;
+            case KEY_PAGE_UP:
+                printf("<PG UP>");
+                break;
+            case KEY_PAGE_DOWN:
+                printf("<PG DOWN>");
+                break;
+            default:
+                printf("<0x%x>",e.code);
+                break;
+        }
     }
     return -1;
 }
