4a1639adbf138821be60009bb8a07f4b3b2ceb39
Tiarnach O Riada <tiarnachreidy@gmail.com>
2020-06-14 23:31:36 +0100

    Identity paging works

--- kernel/arch/i386/paging.c
+++ kernel/arch/i386/paging.c
@@ -5,54 +5,8 @@
 
 #include <sys/debug.h>
 
-/*
-void init_paging()
-{
-    int i = 0;
-    for (; i < 1024; i++)
-    {
-        page_directory[i].page_table_addr = 0;
-        page_directory[i].free = 0;
-        page_directory[i].G = 0;
-        page_directory[i].S = 0;
-        page_directory[i].Z = 0; // always set to 0
-        page_directory[i].A = 0;
-        page_directory[i].D = 0;
-        page_directory[i].W = 0;
-        page_directory[i].U = 0; // Supervisor only
-        page_directory[i].R = 1; // set r/w bit
-        page_directory[i].P = 0; // not present
-    }
-
-    unsigned int phys_addr = 0; // first page physical address
-
-    for(i = 0; i < 1024; i++)
-    {
-        page_table[i].page_addr = phys_addr + 1024 * i;
-        page_table[i].free = 0;
-        page_table[i].G = 0;
-        page_table[i].Z = 0; // Always set to 0
-        page_table[i].D = 0;
-        page_table[i].A = 0;
-        page_table[i].C = 0;
-        page_table[i].W = 0;
-        page_table[i].U = 0;
-        page_table[i].R = 1;
-        page_table[i].P = 1;
-    }
-
-    page_directory[0].page_table_addr = (int) &page_table;
-    page_directory[0].R = 1;
-    page_directory[0].P = 1;
-
-    loadPageDirectory((unsigned int *) &page_directory);
-    serial_writed((unsigned int)page_directory[0]);
-    enablePaging();
-}
-*/
-
-struct page_directory_entry page_directory[1024] __attribute__((aligned(4096)));
-struct page_table_entry page_tables[1024][1024] __attribute__((aligned(4096)));
+uint32_t page_directory[1024] __attribute__((aligned(4096)));
+uint32_t page_tables[1024][1024] __attribute__((aligned(4096)));
 
 extern void load_new_page_directory(uint32_t page_directory_address);
 //extern void load_page_directory(uint32_t page_directory_address);
@@ -61,48 +15,28 @@ extern void load_new_page_directory(uint32_t page_directory_address);
 void paging_init()
 {
     // Identity map everything
-    for (int i = 0; i < 1024; i++)
-    {
-        page_directory[i].page_table_addr = 0;
-        page_directory[i].free  = 0;
-        page_directory[i].S     = 0;
-        page_directory[i].Z     = 0;
-        page_directory[i].A     = 0;
-        page_directory[i].PCD   = 0;
-        page_directory[i].PWT   = 0;
-        page_directory[i].US    = 0;
-        page_directory[i].RW    = 1;
-        page_directory[i].P     = 0;
-    }
-
     for (uint32_t j = 0; j < 1024; j++)
     {
         for (uint32_t i = 0; i < 1024; i++)
         {
-            page_tables[j][i].page_addr = (((j<<22) + (i << 12)) & 0xFFFFF000) >> 12;
-            page_tables[j][i].free = 0;
-            page_tables[j][i].G    = 0;
-            page_tables[j][i].PAT  = 0;
-            page_tables[j][i].D    = 0;
-            page_tables[j][i].A    = 0;
-            page_tables[j][i].PCD  = 0;
-            page_tables[j][i].PWT  = 0;
-            page_tables[j][i].US   = 0;
-            page_tables[j][i].RW   = 1;
-            page_tables[j][i].P    = 1;
+            page_tables[j][i] = 
+                (((j<<22) + (i << 12)) & 0xFFFFF000)
+                | (1 << 1)  // writes allowed
+                | (1 << 0); // present
         }
-        page_directory[j].page_table_addr = (((uint32_t)page_tables[j]) & 0xFFFFF000) >> 12;
-        page_directory[j].P = 1;
+        page_directory[j] = (((uint32_t)page_tables[j]) & 0xFFFFF000)
+            | (1 << 1)
+            | (1 << 0);
     }
 
     printf("Paging initialised with page directory at %x\n", (int)page_directory);
     printf("%d %dB PDEs, each with %d %dB PTEs, total %d kiB (%d MiB).\n"
             ,1024
-            ,sizeof(struct page_directory_entry)
+            ,sizeof(page_directory[0])
             ,1024
-            ,sizeof(struct page_table_entry)
-            ,1024*1024*sizeof(struct page_table_entry)*sizeof(struct page_directory_entry) / (1<<10)
-            ,1024*1024*sizeof(struct page_table_entry)*sizeof(struct page_directory_entry) / (1<<20)
+            ,sizeof(page_tables[0][0])
+            ,1024*1024*sizeof(page_tables[0][0])*sizeof(page_directory[0]) / (1<<10)
+            ,1024*1024*sizeof(page_tables[0][0])*sizeof(page_directory[0]) / (1<<20)
           );
     printf("|PDE#    | PDE    | &PT    | PTE    | &PF    |\n"
            "|--------+--------+--------+--------+--------|\n");
@@ -110,10 +44,11 @@ void paging_init()
     {
         printf("|%.8d|%.8X|%.8X|%.8X|%.8X|\n"
                 ,i
-                ,(page_directory[i])
-                ,page_directory[i].page_table_addr<<12
-                ,(page_tables[i][0])
-                ,((struct page_table_entry*)(page_directory[i].page_table_addr<<12))->page_addr<<12
+                ,page_directory[i]
+                ,page_directory[i] & 0xFFFFF000
+                ,*(uint32_t*)(page_directory[i] & 0xFFFFF000)//page_tables[i][0]
+                ,*(uint32_t*)(page_directory[i] & 0xFFFFF000) & 0xFFFFF000
+                //,((struct page_table_entry*)(page_directory[i].page_table_addr<<12))->page_addr<<12
                 );
     }
     printf("|PDE#    | PDE    | &PT    | PTE    | &PF    |\n"
@@ -122,11 +57,10 @@ void paging_init()
     {
         printf("|%.8d|%.8X|%.8X|%.8X|%.8X|\n"
                 ,0
-                ,*page_directory
-                ,((struct page_table_entry*)((page_directory[0].page_table_addr<<12)+i*sizeof(struct page_table_entry)))
-                ,*((struct page_table_entry*)((page_directory[0].page_table_addr<<12)+i*sizeof(struct page_table_entry)))
-                //,(page_tables[0][i])
-                ,page_tables[0][i].page_addr << 12
+                ,page_directory[0]
+                ,page_directory[0] & 0xFFFFF000
+                ,*(uint32_t*)((page_directory[0] & 0xFFFFF000) + i*sizeof(page_tables[0][0]))
+                ,*(uint32_t*)((page_directory[0] & 0xFFFFF000) + i*sizeof(page_tables[0][0])) &0xFFFFF000 
                 );
                 //,((struct page_table_entry**)(page_directory[0].page_table_addr<<12))[i]->page_addr<<12);
     }
