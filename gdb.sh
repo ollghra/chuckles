045036f5047b42da29e0a4f8980f36296004ff1a
Tiarnach O Riada <tiarnachreidy@gmail.com>
2020-06-21 12:16:32 +0100

    Updated gdb script

--- gdb.sh
+++ gdb.sh
@@ -3,9 +3,9 @@ set -e
 . ./iso.sh
 
 echo "
-gdb
-(gdb) target remote localhost:1234 
+gdb --symbols=isodir/boot/chuckles.kernel --eval-command 'target remote localhost:1234'
 "
 
 #~/src/cpulimit/src/cpulimit -l 5 qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom chuckles.iso -m 256M -monitor stdio -d int
 qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom chuckles.iso -d int -s -S
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -machine pc,accel=kvm  -cdrom chuckles.iso -serial file:serial.log -s -S -d int,pcall,guest_errors,cpu_reset -m 128 -D qemu.log 2>&1
